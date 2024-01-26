493da8eef4a2bd1556fc59c8f66a93790b595569
3x10RaiseTo8 <abhishekcy17@gmail.com>
2024-01-27 02:53:09 +0530

    Add dependencies and configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,27 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+  themes: [
+    {
+      mytheme: {
+        primary: '#ff00f0',
+        secondary: '#00ad00',
+        accent: '#77b100',
+        neutral: '#020504',
+        'base-100': '#fff7ff',
+        info: '#00d8ff',
+        success: '#c1ec00',
+        warning: '#ff7400',
+        error: '#f50000',
+        body: { 'background-color': '#e3e6e6' },
       },
     },
-  },
-  plugins: [],
+  ],
+  plugins: [require('daisyui')],
 };
 export default config;
